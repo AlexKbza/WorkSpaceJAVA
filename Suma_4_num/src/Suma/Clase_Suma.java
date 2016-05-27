2bdeb1597708c2417c0effc97f925cff6fd5b8b3
Denis Cabeza Valencia <maquia2796@gmail.com>
2016-05-27 01:21:42 -0500

    Update Suma_4_num

--- Suma_4_num/src/Suma/Clase_Suma.java
+++ Suma_4_num/src/Suma/Clase_Suma.java
@@ -1,9 +1,43 @@
 package Suma;
 
-public class Clase_Suma {
+import javax.swing.JOptionPane;
 
-	public void compara(String a, String b){
-		if(!a.equals(b))
-			
+public class Clase_Suma {
+	int numero[] = new int[4], num;
+	
+	public Clase_Suma(){}
+	
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num, int indice) {
+		this.num = num;
+		this.numero[indice] = num;
+	}
+
+	public int compara(){
+		int total = 0, x, y, aux=0;
+		for(x=0; x<numero.length; x++){aux=0;
+			for(y=0; y<numero.length; y++){
+				if(x != y){
+					if(numero[x] == numero[y]){
+						aux=1;
+						numero[y] = 0;
+					}
+				}
+			}
+			if(aux == 1)
+				numero[x]=0;
+		}
+		
+		for( x=0; x<numero.length; x++)
+			total += numero[x];
+		
+		return total;
+	}
+	
+	protected void finalize(){
+		JOptionPane.showMessageDialog(null, "El programa se va a cerrar");
 	}
 }
--- Suma_4_num/src/Suma/main.java
+++ Suma_4_num/src/Suma/main.java
@@ -1,10 +1,19 @@
 package Suma;
 
+import javax.swing.JOptionPane;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Clase_Suma numero = new Clase_Suma();
+		for(int x=0; x<numero.numero.length; x++){
+			do{
+				numero.setNum(Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero en la pocision  "+ (x+1) +":")), x);
+			}while(numero.getNum()<0);
+		}
+		JOptionPane.showMessageDialog(null, "El resultado es:\n" + numero.compara());
+		numero = null;
+		System.gc();
 	}
 
 }
